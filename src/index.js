616fd9110bb97f51f39ac3037dc7d0916a003106
martnght12 <martnght@gmail.com>
2021-01-25 20:41:59 -0300

    Configurando Sockets e adicionando eventos

--- src/index.js
+++ src/index.js
@@ -5,13 +5,37 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
+//pasta 'public'
+const public = path.join(__dirname, 'public');
+
+//Entrega os arquivos da pasta 'public' para o express
+app.use(express.static(public));
+
+//Define as views como HTML
+app.set('views', path.join(public));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
+//Rota principal
 app.use('/', (req, res) => {
   res.render('index.html');
 });
 
-server.listen(3000);
+let messages = [];
+
+io.on('connection', (socket) => {
+  console.log('a user has been connected');
+
+  socket.emit('previousMessage', messages);
+
+  socket.on('sendMessage', (data) => {
+    messages.push(data);
+    io.emit('receivedMessage', data);
+  });
+});
+
+
+//Porta do servidor
+server.listen(3000, () => {
+  console.log('listen on *:3000');
+});
